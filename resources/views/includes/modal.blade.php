481706e24fb8a3bf64c5c1bda79b0491d10a1883
double prince <adenekanmartin@gmail.com>
2019-08-10 07:56:59 +0100

    Include missing class:  modal-dialog-centered to affected modals

--- resources/views/includes/modal.blade.php
+++ resources/views/includes/modal.blade.php
@@ -40,8 +40,8 @@
 </div>
 
 <!-- DATA Modal -->
-<div class="modal fade" id="buyData"  tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog" role="document">
+<div class="modal fade " id="buyData"  tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
         <div class="modal-content">
 
             <div class="modal-header border-bottom-0">
@@ -146,7 +146,7 @@
 
 <!-- CABLE TV  modal -->
 <div class="modal fade" id="cabletv"   tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog" role="document">
+    <div class="modal-dialog modal-dialog-centered" role="document">
         <div class="modal-content">
             <div class="modal-header border-bottom-0">
                 <h5 class="modal-title" id="exampleModalLabel">Recharge CABLE TV</h5>
